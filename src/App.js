da1baf4ce470d947c0a0335df225bb527b9e8eb3
Mark Shepherd <mark.e.shepherd@gmail.com>
2020-04-05 15:37:27 -0700

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import HelloWorld from './HelloWorld';
+import Foo from './Foo';
 import './App.css';
+import Mortgage from './mortgage2';
+import { List, ListItem, ListItemText } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <HelloWorld />
-    </div>
-  );
+
+function makeList(listItemClick) {
+	const data = [{name: "abc", cases: 123}, {name: "defgh", cases: 55}, {name: "IUiasf", cases: 932}];
+	return <List class="myList">
+      	{data.map((datum) => {
+      		return (<ListItem button key={datum.name} dense="true" onClick={listItemClick}>
+    			<ListItemText key={datum.name} primary={datum.name} />
+  			</ListItem>);
+        })}
+   </List>;
+}
+
+function calcOptions() {
+	let mortgage = new Mortgage(99, 99, 99);
+	return <React.Fragment>
+				<option value="grapefruit">Grapefruit</option>
+				<option value="lime">Lime</option>
+				<option value="coconut">Coconut</option>
+				<option value="mango">Mango</option>
+			</React.Fragment>;
+}
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {county: "Marin"};
+	}
+
+	myListItemClick(e) {
+		console.log(e);
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<HelloWorld />
+				
+				{true || <select className="mybutton3" defaultValue="mango">{calcOptions()}</select>}
+
+				{true || <p>{this.state.county}</p>}
+
+				{true || makeList(this.myListItemClick)}
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,4 +1,23 @@
 import React from 'react';
+import EnhancedTable from './EnhancedTable';
+import Button from '@material-ui/core/Button';
+
+import Table from '@material-ui/core/Table';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import TableContainer from '@material-ui/core/TableContainer';
+
+const stateNames = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware",
+"Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine",
+"Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada",
+"New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma",
+"Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont",
+"Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
+
+// trying to make the table entry height as small as possible. I got some ideas from
+// https://stackoverflow.com/questions/39210565/material-ui-change-rows-height-and-padding-in-table
+const slimStyle = {height: "0px", padding: "0px"};
+const tableSizeStyle = {height: "500px", width: "250px", maxHeight: "500px"};
 
 const HelloWorld = () => {
   
@@ -6,8 +25,37 @@ const HelloWorld = () => {
     alert('Hello, World!');
   }
   
+  function listItemClick(e) {
+
+  }
+
+  function makeTable() {
+	return <TableContainer style={tableSizeStyle}>
+				<Table style={slimStyle} size="small">
+  					{stateNames.map((stateName) => {
+				  		return <TableRow style={slimStyle} onClick={listItemClick}>
+							<TableCell style={slimStyle} component="th" scope="row" padding="none">
+				            	{stateName}
+				            </TableCell>
+							<TableCell style={slimStyle} align="right">{stateName.length}</TableCell>
+							<TableCell style={slimStyle} align="right">{stateName.length * stateName.length}</TableCell>
+				  		</TableRow>
+				    })}
+				</Table>
+			</TableContainer>;
+  }
+
   return (
-    <button onClick={sayHello}>Click me!</button>
+  	<div>
+  		{true || <EnhancedTable/>}
+	    {true || <button onClick={sayHello}>xxxxClick me!</button>}
+	    {true || <br/>}
+	    {true || <Button variant="contained" color="primary">Hello World</Button>}
+
+	    <div style={tableSizeStyle}>
+		    {makeTable()}
+		</div>
+	</div>
   );
 };
 
