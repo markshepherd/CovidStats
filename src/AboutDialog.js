10a6dd57cc4f44e504dc1d9730652f36b60cec26
Mark Shepherd <mark.e.shepherd@gmail.com>
2020-04-14 19:21:39 -0700

    fix layout bug on safari

--- src/AboutDialog.js
+++ src/AboutDialog.js
@@ -4,15 +4,21 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Analytics from './Analytics';
 
 export default class AboutDialog extends React.Component {
 	handleCloseButton = () =>{
 		this.props.onCloseButton();
 	}
 
+	noop = (something) => {
+		Analytics.aboutDialogOpened();
+		return something;
+	}
+
 	render() {
 		return (
-			<Dialog onClose={this.handleCloseButton} open={this.props.open} aria-labelledby="customized-dialog-title">
+			<Dialog onClose={this.handleCloseButton} open={this.noop(this.props.open)} aria-labelledby="customized-dialog-title">
 				<DialogTitle id="customized-dialog-title" onClose={this.handleCloseButton}>
 					Covid-19 Statistics for U.S. States & Counties 
 				</DialogTitle>
