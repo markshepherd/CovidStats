848c881babfa365b89535c15e55bbcc2d4de33eb
Mark Shepherd <mark.e.shepherd@gmail.com>
2020-04-16 18:09:51 -0700

    add facebook and email icons, adopt a 'no dots' version number scheme.

--- src/App.js
+++ src/App.js
@@ -13,11 +13,10 @@ import MyLink from './MyLink';
 import RegionMenu from './RegionMenu';
 import RegionTable from './RegionTable';
 import SeriesChart from './SeriesChart';
-// import Utils from './Utils';
 import { version } from './Version';
-
 import './App.css';
 
+const versionString = version.split(/\./)[0].toString();
 const development = window.location.toString().match(/(localhost|covid-test)/)
 Analytics.enable(!development);
 
@@ -300,8 +299,8 @@ class App extends React.Component {
 			}
 
 			<div className="notesText">
-				<p>Created by 
-				<MyLink target="_blank" href="mailto:markcharts591@gmail.com"> Mark Shepherd</MyLink>.
+				<p>Created by
+				Mark Shepherd. {/*<MyLink href="#" onClick={this.handleClickMe}> Mark Shepherd</MyLink>.*/}
 				Data provided by the <MyLink target="_blank" href="https://github.com/nytimes/covid-19-data"> New York Times</MyLink>.
 				Source code is <MyLink target="_blank" href="https://github.com/markshepherd/CovidStats"> here</MyLink>.
 				</p>
@@ -320,8 +319,20 @@ class App extends React.Component {
                     className="socialIconH" align="right" alt="Visit the Larkdales">
                          <path d="M26.451.526C12.155.526.565 12.116.565 26.412s11.59 25.886 25.886 25.886 25.886-11.59 25.886-25.886S40.748.526 26.451.526zM40.005 27.14h-2.689v9.918c0 .718-.026 1.299-1.014 1.299h-6.574V28.41h-6.554v9.947h-6.263c-1.295 0-1.326-.581-1.326-1.299V27.14h-2.689c-.96 0-1.206-.56-.547-1.244l12.903-12.915a1.659 1.659 0 012.399 0l12.902 12.915c.659.684.413 1.244-.548 1.244z"></path></svg>
                 </MyLink>
+				<MyLink target="_blank" href="https://facebook.com/mark.e.shepherd">
+				 	<img className="socialIconF"
+				 		align="right"
+				 		alt="Visit Mark on Facebook"
+				  		src="FB_logo_RGB-Black_1024.svg"/>
+				</MyLink>
+				<MyLink target="_blank" href="mailto:markcharts591@gmail.com">
+				 	<img className="socialIconE"
+				 		align="right"
+				 		alt="Send email to Mark"
+				  		src="email-seeklogo.svg"/>
+				</MyLink>
                 <Typography variant="caption">
-					{development ? "DEV" : "PROD"} {version}, {buildDate}
+					{development ? "DEV" : "PROD"} {versionString}, {buildDate}
 				</Typography>
 			</div>
 		</React.Fragment>;
--- src/MyLink.js
+++ src/MyLink.js
@@ -3,8 +3,9 @@ import {Link} from '@material-ui/core';
 import Analytics from './Analytics';
 
 export default class MyLink extends React.Component {
-	handleClick = () => {
+	handleClick = (e) => {
 		Analytics.reportOutboundLink(this.props.href);
+		this.props.onClick && this.props.onClick(e);
 	}
 
 	render() {
